391fc84ab2cedcc9b99896d4fa815acb5fd9e04f
sathyaanu <anuanuja12@gmail.com>
2016-09-09 20:42:09 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public Cds
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int i=1;
+int fact=1;
+while(i<=a)
+{
+fact=fact*i;
+i++;
+}
+System.out.println(fact);
+}
