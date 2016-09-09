3d10feb74c4183addd36a93aef556db0fb5e0d32
sathyaanu <anuanuja12@gmail.com>
2016-09-09 21:10:23 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Was
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int count=0;
+int j=0;
+for(int i=2;i<=n;i++)
+{
+if(n%i==0)
+{
+count++;
+}
+if(count==2)
+{
+j++;
+}
+}
+System.out.println(i);
+}
