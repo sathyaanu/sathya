fe491e8bc77167dfdd40a43da22afe7ef58cc81a
sathyaanu <anuanuja12@gmail.com>
2016-09-09 11:30:05 +0530

    Create pro.java

---
+++ pro.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public Csd
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0)
+System.out.println("Zero");
+else if(n>0)
+System.out.println("Positive value");
+else
+System.out.println("Negative value");
+}
