a3e50e99c8ecd1983003ead62e251851cdb8521a
sathyaanu <anuanuja12@gmail.com>
2016-09-14 23:46:15 +0530

    Create Num.java

---
+++ Num.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Num
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(n>0>
+{
+print("Positive number");
+}
+else if(n<0)
+{
+print("Negative number");
+}
+else
+{
+print("Zero");
+}
+}
+}
