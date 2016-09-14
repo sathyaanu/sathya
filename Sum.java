b6586cbef5f5c70f1e59472a165eeea215404a8a
sathyaanu <anuanuja12@gmail.com>
2016-09-15 00:16:29 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Sum
+{
+public static void main(String[] args)
+{
+Scanner w=new Scanner(System.in);
+int j=w.nextInt();
+int[] f=new int[j];
+int sum=0;
+for(int t=o;t<j;t++)
+{
+f[t]=w.nextInt();
+}
+for(int b=0;b<j;b++)
+{
+sum+=f[b];
+}
+System.out.println(sum);
+}
+}
