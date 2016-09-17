b7d588c00c7bce9deb5a913281b2c10e6cc635d7
SATHYAPRIYA D <anuanuja12@gmail.com>
2016-09-17 22:00:22 +0530

    Create Wordcountbig.java

---
+++ Wordcountbig.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Wordcountbig
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+int r=a.length;
+System.out.println(r);
+}
+}
