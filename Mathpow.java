d414dc86801ba1836c84a22767cab038c6e81e38
SATHYAPRIYA D <anuanuja12@gmail.com>
2016-09-17 21:17:33 +0530

    Create Mathpow.java

---
+++ Mathpow.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Mathpow
+{
+public static void main(String[] args)
+{
+Scanner d=new Scanner(System.in);
+int f=d.nextInt();
+int p=d.nextInt();
+System.out.println(Math.pow(f,d);
+}
+}
