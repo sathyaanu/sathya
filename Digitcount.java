ac6deeb4e731db8ff7ce5a16248a288dd5d65fae
SATHYAPRIYA D <anuanuja12@gmail.com>
2016-09-17 21:12:10 +0530

    Create Digitcount.java

---
+++ Digitcount.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Digitcount
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=a.length();
+System.out.println(b);
+}
+}
